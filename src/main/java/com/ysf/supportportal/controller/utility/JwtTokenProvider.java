cbaba701b274a7e27f4527a913f77bff1e3346a7
ysfmetu <ysf_metu@hotmail.com>
2022-08-13 17:23:38 +0300

    adminportal sistemi *jwtToken oluşturmak için ilk metodumuz yazıldı   generateJwtToken() metodu ile token içinde koyacağımız parametreler token içine konulup imzalama işlemi gerçekleşmiştir.   *yml dosyası içerisine imzada kullanacağımız secret key için değer atanıp @Value anotasyonu ile çağırıldı

--- src/main/java/com/ysf/supportportal/controller/utility/JwtTokenProvider.java
+++ src/main/java/com/ysf/supportportal/controller/utility/JwtTokenProvider.java
@@ -1,13 +1,31 @@
 package com.ysf.supportportal.controller.utility;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.ysf.supportportal.constant.SecurityConstant;
 import com.ysf.supportportal.domain.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class JwtTokenProvider {
     @Value("jwt.secret")
     private String secret;
 
     public String generateJwtToken(UserPrincipal userPrincipal){
-        return "";
+        String[] claims=getClaimsFromUser(userPrincipal);
+        return JWT.create()
+                .withIssuer(SecurityConstant.GET_ARRAYS_LLC)
+                .withAudience(SecurityConstant.GET_ARRAYS_ADMINISTRATION)
+                .withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(SecurityConstant.AUTHORITIES,claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() +SecurityConstant.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(secret.getBytes()));
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+        return null;
     }
 }
